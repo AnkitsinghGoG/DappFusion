5c67baffee302b8840a10bc857163cd34e4e64a1
AnkitsinghGoG <ankit.singh@geeksofgurukul.com>
2025-11-25 23:29:27 +0530

    Update deploy.js

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -17,7 +17,6 @@ async function main() {
 
   await hre.run("compile");
 
-
   const ContractFactory = await hre.ethers.getContractFactory("DappFusion");
   const contract = await ContractFactory.deploy();
 
